0ed8c8d0bd383399d76f32e8d568db2b017be5e2
GrishinAnton <grishin_rostov@mail.ru>
2019-09-20 07:07:28 +0300

    Правка тестов

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,6 +1,5 @@
-// https://docs.cypress.io/api/introduction/api.html
-// 1. Написать тест на Cypress, проверяющий корректность механизма добавления задачи
-// 2. Тест, проверяющий корректность работы валидации поля времени
+const TIMER_VALIDATION = 2000;
+const EXAMPLE_TIME_TEST_VALIDATION = [":10", "30:10", "40:", "50:123"];
 
 describe("My First Test", () => {
   it("Visits the app root url", () => {
@@ -11,50 +10,52 @@ describe("My First Test", () => {
     cy.get("[data-cy='task-items']")
       .children()
       .then(e => {
-        let startLength = e.length;
+        let startTasksLength = e.length;
 
         cy.get("[data-cy='add-task-button']")
           .click()
           .get("[data-cy='input-time']")
           .type("10:10")
-          .wait(2500)
+          .wait(TIMER_VALIDATION)
           .get("[data-cy='input-text']")
           .type("Test")
           .get("[data-cy='save-task-button']")
           .click()
           .get("[data-cy='task-items']")
           .children()
-          .should("have.length", ++startLength);
+          .should("have.length", ++startTasksLength);
       });
   });
 
   it("Time validation", () => {
-    cy.get("[data-cy='add-task-button']").click();
+    cy.get("[data-cy='add-task-button']")
+      .click()
+      .wrap(EXAMPLE_TIME_TEST_VALIDATION)
+      .each(num => {
+        return new Cypress.Promise(resolve => {
+          cy.get("[data-cy='input-time']")
+            .clear()
+            .type(num);
 
-    cy.wrap([":10", "30:10", "40:", "50:123"]).each(num => {
-      return new Cypress.Promise(resolve => {
-        cy.get("[data-cy='input-time']").clear();
-        cy.get("[data-cy='input-time']").type(num);
+          cy.wait(TIMER_VALIDATION);
 
-        cy.wait(2500);
-
-        cy.get("[data-cy='input-text']").should("be.disabled");
-        resolve();
-      });
-    });
-    cy.get("[data-cy='input-time']").clear();
-    cy.get("[data-cy='input-time']").type("00:00");
-
-    cy.wait(2500);
-
-    cy.get("[data-cy='input-text']").should("not.be.disabled");
-
-    cy.get("[data-cy='input-time']").clear();
-    cy.wait(500);
-    cy.get("[data-cy='input-time']").type("00:00");
-
-    cy.wait(2500);
-
-    cy.get("[data-cy='input-text']").should("not.be.disabled");
+          cy.get("[data-cy='input-text']").should("be.disabled");
+          resolve();
+        });
+      })
+      .get("[data-cy='input-time']")
+      .clear()
+      .type("00:00")
+      .wait(TIMER_VALIDATION)
+      .get("[data-cy='input-text']")
+      .should("not.be.disabled")
+      .get("[data-cy='input-time']")
+      .clear()
+      .wait(500)
+      .type("00:00")
+      .get("[data-cy='input-text']")
+      .should("be.disabled")
+      .wait(TIMER_VALIDATION)
+      .should("not.be.disabled");
   });
 });
